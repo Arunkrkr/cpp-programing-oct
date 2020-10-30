8739a59bb230a6678e74995456723731e0a8c12f
Arunkrkr <69425162+Arunkrkr@users.noreply.github.com>
2020-10-30 16:33:03 +0530

    Add files via upload
    if ur eligible to vote or not

---
+++ pgm 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout << "Enter ur age ";
+    cin >>age;  
+    if(age>18)
+	{
+		cout<<"u are eligible to vote";
+	}  
+	else 
+	{
+	cout<<"u are not eligible to vote";	
+	}
+return 0;
+}
