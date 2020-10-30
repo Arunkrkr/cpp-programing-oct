d83c58c9be4f5b2ff9aa6406d6f23d485d219f7e
Arunkrkr <69425162+Arunkrkr@users.noreply.github.com>
2020-10-30 16:26:03 +0530

    Add files via upload
    product of the given numbers

---
+++ pgm 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a,b,pro;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;  
+    pro = a * b;  
+
+    cout << "Product = " << pro;    
+    
+    return 0;
+}
